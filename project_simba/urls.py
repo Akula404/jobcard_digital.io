2e4989b3bcfc8359cfd7041c034a13b9ede2b32e
Akula404 <berrycarloschernobyl@gmail.com>
2026-03-09 16:21:06 +0300

    Add root URL redirect

--- project_simba/urls.py
+++ project_simba/urls.py
@@ -1,19 +1,17 @@
-"""
-URL configuration for project_simba project.
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/jobcard/')
 
 urlpatterns = [
+    path('', home),   # 👈 add this line
     path('admin/', admin.site.urls),
-
-    # Jobcard system URLs
     path('jobcard/', include(('jobcard.urls', 'jobcard'), namespace='jobcard')),
 ]
 
-# Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
