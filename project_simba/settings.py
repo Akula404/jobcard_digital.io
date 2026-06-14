9a67e7ff6eab2a99a327cdbb588cca70ee09acf2
Akula404 <berrycarloschernobyl@gmail.com>
2026-06-14 07:35:57 -0700

    Logins-3

--- project_simba/settings.py
+++ project_simba/settings.py
@@ -124,6 +124,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/jobcard/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
